0f50aae3783092f49d2196e53cfda38696c06a58
Daniel Mikes <max8@post.cz>
2021-08-24 12:13:02 +0200

    refactor

---
+++ RPS.py
@@ -0,0 +1,12 @@
+import random
+
+#moves
+move = ('R','P','S')
+
+# this bot makes random moves
+def player(prev_play, opponent_history=[]):
+
+  # generate random move
+  guess = move[random.randint(0,2)]
+
+  return guess
--- RPS1.py
+++ RPS1.py
@@ -20,22 +20,12 @@ next_mat = [
   [[0,0,0],[0,0,0],[0,0,0]]
 ]
 
-# this bot maps all permutations (9) of two move sequences
-# for each counts occurrence of third move options (3)
-# predicts third move
-# chooses best counter move
-def player(prev_play, opponent_history=[]):
-  opponent_history.append(prev_play)
-
-  # generate random move
-  guess = move[random.randint(0,2)]
-
+# add last move to last_mat and count to next_mat
+def addLastTwo(prev_play, opponent_history=[]):
   last_one = ""
-  last_two = ""
-  prev_two = ""
   if len(opponent_history) >= 1:
     last_one = opponent_history[-1]
-
+  prev_two = ""
   # add last move to respective counter in next_mat
   if len(opponent_history) >= 3:
     prev_two = "".join(opponent_history[-3:-1])
@@ -49,22 +39,52 @@ def player(prev_play, opponent_history=[]):
               # raise count of last move for previous last two
               next_mat[i][j][k] += 1
 
+# predict next move from occurrence in next_mat
+def predictNext(prev_play, opponent_history=[]):
+  last_one = ""
+  if len(opponent_history) >= 1:
+    last_one = opponent_history[-1]
   # find index of last two
-  if len(opponent_history) >= 2:
-    last_two = "".join(opponent_history[-2:])
-    for i in range(3):
-      for j in range(3):
-        # find index of last two
-        if last_mat[i][j] == last_two:
-          # find expected third move
-          next_max = max(next_mat[i][j])
-          if next_max == 0:
-            # generate random move
-            guess = move[random.randint(0,2)]
-          else:
-            next_index = next_mat[i][j].index(next_max)
-            next_move = move[next_index]
-            # make best response move
-            guess = next_best[next_move]
+  else:
+    last_two = ""
+    if len(opponent_history) >= 2:
+      last_two = "".join(opponent_history[-2:])
+      for i in range(3):
+        for j in range(3):
+          # find index of last two
+          if last_mat[i][j] == last_two:
+            # find expected third move
+            next_max = max(next_mat[i][j])
+            if next_max == 0:
+              # generate random move
+              guess = move[random.randint(0,2)]
+            else:
+              next_index = next_mat[i][j].index(next_max)
+              next_move = move[next_index]
+              # make best response move
+              guess = next_best[next_move]
+
+# this bot maps all permutations (9) of two move sequences
+# for each counts occurrence of third move options (3)
+# predicts third move
+# chooses best counter move
+def player(prev_play, opponent_history=[]):
+  opponent_history.append(prev_play)
+
+  # add last two moves to last_mat
+  addLastTwo(opponent_history)
+
+  # todo: add random moves
+  # ratio of naive / smart action
+  beta = 0.5
+  # generate choice between naive and smart action
+  alpha = random.rand()
+  if alpha > beta:
+    # predict next move from next_mat
+    smart_move
+    predictNext(opponent_history)
+  else:
+    # generate random move
+    guess = move[random.randint(0,2)]
 
   return guess
